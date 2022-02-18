841660db6daf2ac0de3d5ab51c4951477e9c0c61
faithdevs <ugbeshefaith@gmail.com>
2022-02-18 10:18:36 +0100

    new

--- src/controllers/chatBotController.js
+++ src/controllers/chatBotController.js
@@ -60,7 +60,7 @@ let postWebhook = (req, res) => {
 
 let getWebhook = (req, res) => {
     // Your verify token. Should be a random string.
-    let VERIFY_TOKEN = process.env.VERIFY_TOKEN;
+    let VERIFY_TOKEN = process.env.MY_VERIFY_FB_TOKEN;
 
     // Parse the query params
     let mode = req.query['hub.mode'];
