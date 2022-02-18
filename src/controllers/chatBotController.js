8b814e74b5e10766ff09943847018dd6dfb9f8af
faithdevs <ugbeshefaith@gmail.com>
2022-02-18 13:26:06 +0100

    new

--- src/controllers/chatBotController.js
+++ src/controllers/chatBotController.js
@@ -172,7 +172,7 @@ function handleMessage(sender_psid, message) {
         return;
     }
 
-    let entitiesArr = [ "wit$greetings", "wit$thanks", "wit$bye" ];
+    let entitiesArr = [ "greetings", "thanks", "bye" ];
     let entityChosen = "";
     entitiesArr.forEach((name) => {
         let entity = firstTrait(message.nlp, name);
@@ -185,15 +185,15 @@ function handleMessage(sender_psid, message) {
         //default
         callSendAPI(sender_psid,`The bot is needed more training, try to say "thanks a lot" or "hi" to the bot` );
     }else{
-       if(entityChosen === "wit$greetings"){
+       if(entityChosen === "greetings"){
            //send greetings message
            callSendAPI(sender_psid,'Hi there! This bot is created by Hary Pham. Watch more videos on HaryPhamDev Channel!');
        }
-       if(entityChosen === "wit$thanks"){
+       if(entityChosen === "thanks"){
            //send thanks message
            callSendAPI(sender_psid,`You 're welcome!`);
        }
-        if(entityChosen === "wit$bye"){
+        if(entityChosen === "bye"){
             //send bye message
             callSendAPI(sender_psid,'bye-bye!');
         }
